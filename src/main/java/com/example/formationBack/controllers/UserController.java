557ee5616be806e9ba6fc67fe785dba1777074d7
Marion <marion.koosinlin@apside-groupe.com>
2020-10-15 16:44:45 +0200

    add updateProfile request + fix getProfile

--- src/main/java/com/example/formationBack/controllers/UserController.java
+++ src/main/java/com/example/formationBack/controllers/UserController.java
@@ -1,16 +1,14 @@
 package com.example.formationBack.controllers;
 
-import java.util.Optional;
+import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.formationBack.models.Profile;
@@ -26,16 +24,41 @@ public class UserController {
 	
 	// récupère profil de l'utilisateur
 	@GetMapping("/profile/{username}")
-	public Profile getProfile(@PathVariable("username") String username) throws Exception {
+	public Profile getProfile(@PathVariable("username") String username, Principal principal) throws Exception {
 		
-		try {
-			Optional<User> user = userRepository.findByUsername(username);
-			Profile profile = new Profile(user.get().getUsername(), user.get().getEmail());
-			return profile;
-			
-		} catch (BadCredentialsException e) {
-			throw new Exception("This user profile does not exist", e);
+		if(username.equals(principal.getName())) {
+			try {
+				User user = userRepository.findByUsername(username);
+				Profile profile = new Profile(user.getUsername(), user.getEmail());
+				return profile;
+				
+			} catch (BadCredentialsException e) {
+				throw new Exception("This user profile does not exist", e);
+			}
+		} else {
+			throw new Exception("Error. You do not have access to these datas or problem happen when trying to retrieve your data");
 		}
+	
+		
 	}
+	
+	//modifie info du profil
+	@PutMapping("/profile/{username}")
+	public String updateProfile(@PathVariable("username") String username, Principal principal, @RequestBody Profile profile) throws Exception {
+		if(username.equals(principal.getName())) {
+			try {
+				User user = userRepository.findByUsername(username);
+				 user.setEmail(profile.getEmail());
+				userRepository.save(user);
+				return "user updated";
+			} catch (BadCredentialsException e) {
+				throw new Exception("Error when updating this profile. Please try again", e);
+			}
+		} else {
+			throw new Exception("Error.You can only update your profile");
+		}
+			
+	}
+	
 
 }
