d4f0ab8f56f46bf3fb9c0e9a5624daec4ccb0c8a
Marion <marion.koosinlin@apside-groupe.com>
2020-10-28 09:58:01 +0100

    update password

--- src/main/java/com/example/formationBack/models/UpdatePassword.java
+++ src/main/java/com/example/formationBack/models/UpdatePassword.java
@@ -2,21 +2,13 @@ package com.example.formationBack.models;
 
 public class UpdatePassword {
 	
-	private String oldPassword;
 	private String password;
 	
 	
 	public UpdatePassword(String oldPassword, String password) {
-		this.oldPassword = oldPassword;
 		this.password = password;
 	}
 	
-	public String getOldPassword() {
-		return oldPassword;
-	}
-	public void setOldPassword(String oldPassword) {
-		this.oldPassword = oldPassword;
-	}
 	public String getPassword() {
 		return password;
 	}
